a5b737702cbd501f0480458cba5f1c63189ef9ea
gpbacay <giannebacay2004@gmail.com>
2024-12-07 13:18:59 +0800

    "Added long_description_content_type to setup.py"

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='giannebacay2004@gmail.com',
     description='A Python library for custom neuromorphic neural network mechanisms built on top of TensorFlow and Keras',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/gpbacay/gpbacay_arcane',
     packages=find_packages(),
     install_requires=[
