862b79485486e63f5cfe4eb960db006127ab50e2
gpbacay <giannebacay2004@gmail.com>
2024-12-10 15:04:41 +0800

    `Refactor DSTSMGSER class and remove redundant code in main.py`

--- gpbacay_arcane.py
+++ gpbacay_arcane.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.layers import Dense, Dropout, LayerNormalization
-from tensorflow.keras.callbacks import Callback
+from tf.keras.layers import RNN, Input, BatchNormalization, Flatten, Dense, Dropout, LayerNormalization
+from tf.keras.callbacks import Callback
 
 
 
@@ -806,7 +806,7 @@ class PositionalEncodingLayer(tf.keras.layers.Layer):
 
 
 
-class DynamicSelfModelingReservoirCallback(tf.keras.callbacks.Callback):
+class DynamicSelfModelingReservoirCallback(Callback):
     def __init__(self, reservoir_layer, performance_metric='accuracy', target_metric=0.95,
                  growth_rate=10, prune_rate=0.05, performance_threshold=0.01, 
                  growth_phase_length=10, pruning_phase_length=5):
@@ -874,3 +874,91 @@ class DynamicSelfModelingReservoirCallback(tf.keras.callbacks.Callback):
 
 
 
+
+class DSTSMGSER:
+    def __init__(self, input_shape, reservoir_dim, spectral_radius, leak_rate, spike_threshold, max_dynamic_reservoir_dim, output_dim, use_weighted_summary=False):
+        self.input_shape = input_shape
+        self.reservoir_dim = reservoir_dim
+        self.spectral_radius = spectral_radius
+        self.leak_rate = leak_rate
+        self.spike_threshold = spike_threshold
+        self.max_dynamic_reservoir_dim = max_dynamic_reservoir_dim
+        self.output_dim = output_dim
+        self.use_weighted_summary = use_weighted_summary
+        self.model = None
+        self.reservoir_layer = None
+
+    def build_model(self):
+        inputs = Input(shape=self.input_shape)
+
+        # Preprocessing
+        x = BatchNormalization()(inputs)
+        x = Flatten()(x)
+        x = LayerNormalization()(x)
+        x = Dropout(0.2)(x)
+
+        # Attention Layer
+        linear_attention_layer = MultiheadLinearSelfAttentionKernalizationLayer(
+            d_model=128, num_heads=8, use_weighted_summary=self.use_weighted_summary)
+        x = ExpandDimensionLayer()(x)
+        x = linear_attention_layer(x)
+
+        # Reservoir layer
+        self.reservoir_layer = GatedSpikingElasticReservoirLayer(
+            initial_reservoir_size=self.reservoir_dim,
+            input_dim=x.shape[-1],
+            spectral_radius=self.spectral_radius,
+            leak_rate=self.leak_rate,
+            spike_threshold=self.spike_threshold,
+            max_dynamic_reservoir_dim=self.max_dynamic_reservoir_dim
+        )
+        lnn_layer = RNN(self.reservoir_layer, return_sequences=True)
+        lnn_output = lnn_layer(x)
+
+        # Hebbian homeostatic layer
+        hebbian_homeostatic_layer = HebbianHomeostaticLayer(units=self.reservoir_dim, name='hebbian_homeostatic_layer')
+        x = hebbian_homeostatic_layer(lnn_output)
+
+        # Classification output
+        clf_out = DenseReservoirLayer(
+            units=self.output_dim,
+            input_dim=x.shape[-1],
+            spectral_radius=self.spectral_radius,
+            leak_rate=self.leak_rate,
+            spike_threshold=self.spike_threshold,
+            max_dynamic_units=self.max_dynamic_reservoir_dim,
+            activation='softmax',
+            name='clf_out'
+        )(Flatten()(x))
+
+        # Self-modeling output
+        sm_out = DenseReservoirLayer(
+            units=np.prod(self.input_shape),
+            input_dim=x.shape[-1],
+            spectral_radius=self.spectral_radius,
+            leak_rate=self.leak_rate,
+            spike_threshold=self.spike_threshold,
+            max_dynamic_units=self.max_dynamic_reservoir_dim,
+            activation='sigmoid',
+            name='sm_out'
+        )(Flatten()(x))
+
+        # Compile the model
+        self.model = tf.keras.Model(inputs=inputs, outputs=[clf_out, sm_out])
+
+    def compile_model(self):
+        self.model.compile(
+            optimizer='adam',
+            loss={
+                'clf_out': 'categorical_crossentropy',
+                'sm_out': 'mse'
+            },
+            loss_weights={
+                'clf_out': 1.0,
+                'sm_out': 0.5
+            },
+            metrics={
+                'clf_out': 'accuracy',
+                'sm_out': 'mse'
+            }
+        )
