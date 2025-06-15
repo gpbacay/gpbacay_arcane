41b3802366839fab714e2aa0fac6b667467efccf
gpbacay <giannebacay2004@gmail.com>
2025-06-16 02:32:56 +0800

    Update requirements for TensorFlow, modify Vercel configuration for static file handling, and enhance settings.py to allow localhost in ALLOWED_HOSTS.

--- arcane_project/arcane_project/settings.py
+++ arcane_project/arcane_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', 'django-insecure-key-placeholder')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
 
 
 # Application definition
