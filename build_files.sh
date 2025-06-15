5f841a267a722c0aa69570d853e77912d0fbaee5
gpbacay <giannebacay2004@gmail.com>
2025-06-16 02:38:30 +0800

    Enhance build process by adding error handling in build_files.sh, installing Python dependencies, and updating Vercel configuration for static file routing. Remove unnecessary build configurations from vercel.json and streamline package.json scripts.

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Exit on error
+set -e
+
 echo "Building project..."
 
-# Build tailwind
-echo "Building Tailwind CSS..."
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Go into the sub-project directory to handle Node.js dependencies
 cd arcane_project
+
+# Install Node.js dependencies and build CSS
 npm install
 npm run build:css
+
+# Go back to the root directory
 cd ..
 
 # Collect static files
-echo "Collecting static files..."
 python arcane_project/manage.py collectstatic --noinput
 
 echo "Build finished." 
