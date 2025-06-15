bb67d18676c31247747d7fa69c32835e977bb32b
gpbacay <giannebacay2004@gmail.com>
2025-06-16 03:11:44 +0800

    Refactor project structure by removing build_files.sh and arcane_project package.json, and updating main package.json for streamlined build process. Modify vercel.json to adjust routing for Python application.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './arcane_project/recognizer/templates/**/*.html',
+  ],
+  theme: {
+// ... existing code ...
+  }
+} 
